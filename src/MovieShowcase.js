c17a96ac146a2e3e7132c43a635d763e2a6d2cb2
Haloren <mckennakyler@gmail.com>
2021-02-17 23:56:24 -0700

    Done.

--- src/MovieShowcase.js
+++ src/MovieShowcase.js
@@ -3,9 +3,18 @@ import MovieCard from './card-components/MovieCard.js'
 import movieData from './data.js'
 
 export default class MovieShowcase extends Component {
-
   generateMovieCards = () => {
     // map over your movieData array and return an array of the correct JSX
+    return movieData.map((movie) => 
+      // console.log("MovieShowcase", movie)
+      <MovieCard
+        title = {movie.title}
+        IMDBRating = {movie.IMDBRating}
+        genres = {movie.genres}
+        poster = {movie.poster}
+        key = {movie.key}
+      />
+    )
   }
 
   render() {
--- src/data.js
+++ src/data.js
@@ -3,56 +3,65 @@ const movieData = [
     title: 'Choux and Maru go to Istanbul',
     IMDBRating: 3,
     genres: ['cats', 'adventure', 'romance'],
-    poster: 'choux-maru-istanbul'
+    poster: 'choux-maru-istanbul',
+    key: 1
   },
   {
     title: 'Choux and Maru',
     IMDBRating: 4,
     genres: ['cats', 'drama', 'romance'],
-    poster: 'choux-maru-part-1'
+    poster: 'choux-maru-part-1',
+    key: 2
   },
   {
     title: 'ChromeBoi',
     IMDBRating: 5,
     genres: ['thriller', 'romance'],
-    poster: 'chromeboi'
+    poster: 'chromeboi',
+    key: 3
   },
   
   {
     title: 'Escape from VIM',
     IMDBRating: 0,
     genres: ['an awful movie'],
-    poster: 'escape-from-vim'
+    poster: 'escape-from-vim',
+    key: 4
   },
   {
     title: 'GoldenEye',
     IMDBRating: 4,
     genres: ['mystery', 'thriller', 'crime'],
-    poster: 'goldeneye'
+    poster: 'goldeneye',
+    key: 5
   },
   {
     title: 'Handsome Boy Modeling Club',
     IMDBRating: 2,
     genres: ['coming of age', 'romance'],
-    poster: 'handsome-boy'
+    poster: 'handsome-boy',
+    key: 6
   },
   {
     title: "Maru's Spinoff: Trapped in the Sheets",
     IMDBRating: 5,
     genres: ['Adventure'],
-    poster: 'marus-spinoff'
+    poster: 'marus-spinoff',
+    key: 7
   },
   {
     title: 'Terrance: King of the Rats',
     IMDBRating: 1,
     genres: ['drama'],
-    poster: 'terrance-king'
+    poster: 'terrance-king',
+    key: 8
   },
   {
     title: 'The Trash Man',
     IMDBRating: 5,
     genres: ['crime', 'period piece'],
-    poster: 'the-trash-man'
+    poster: 'the-trash-man',
+    key: 9
   },
 ]
 
